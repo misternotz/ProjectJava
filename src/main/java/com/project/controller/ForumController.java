f3ae33bce556b85a8607710b6f93c03f076d508b
PoseiDon <suriya.b@kkumail.com>
2024-02-24 18:20:37 +0700

    แก้นิดหน่อย

--- src/main/java/com/project/controller/ForumController.java
+++ src/main/java/com/project/controller/ForumController.java
@@ -1,6 +1,5 @@
 package com.project.controller;
 
-import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.project.model.Forum;
 import com.project.model.ForumRepo;
 
